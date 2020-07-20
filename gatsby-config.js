883ed561d2fd176e67e9c28434351a97a25be729
mateusz-pocztowski <55945204+mateusz-pocztowski@users.noreply.github.com>
2020-07-20 17:31:18 +0200

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,25 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Where in the world?`,
+    description: `The app that allows user to find each country interesting statistics and information`,
+    author: `@mateusz-pocztowski`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Nunito Sans:300, 600, 800`],
+        display: 'swap',
       },
     },
     `gatsby-transformer-sharp`,
@@ -21,14 +30,28 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/favicon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-react-redux`,
+      options: {
+        pathToCreateStoreModule: './src/redux/store',
+        cleanupOnClient: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        components: path.join(__dirname, 'src/components'),
+        layouts: path.join(__dirname, 'src/layouts'),
+        theme: path.join(__dirname, 'src/theme'),
+        hooks: path.join(__dirname, 'src/hooks'),
+        store: path.join(__dirname, 'src/redux/store'),
+        actions: path.join(__dirname, 'src/redux/actions'),
+        reducer: path.join(__dirname, 'src/redux/reducer'),
+        icons: path.join(__dirname, 'src/assets/icons'),
+      },
+    },
   ],
-}
+};
