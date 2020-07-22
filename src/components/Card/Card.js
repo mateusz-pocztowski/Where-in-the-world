9413202c920c48df340558122e084744da6c8de9
mateusz-pocztowski <55945204+mateusz-pocztowski@users.noreply.github.com>
2020-07-22 19:16:03 +0200

    Added loading skeleton

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,6 +6,7 @@ import slugify from 'slugify';
 
 const Wrapper = styled(Link)`
   display: block;
+  width: 100%;
   max-width: 320px;
   height: 100%;
   border-radius: 8px;
@@ -16,13 +17,15 @@ const Wrapper = styled(Link)`
   border: 2px solid transparent;
   color: ${({ theme }) => theme.text};
   text-decoration: none;
+  opacity: ${({ visible }) => (visible ? '1' : '0')};
+  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
   &:hover {
     border-color: ${({ theme }) => theme.blue};
   }
 `;
 
 const ImageWrapper = styled.div`
-  width: 320px;
+  width: 100%;
   height: 200px;
 `;
 
@@ -43,39 +46,48 @@ const Title = styled.h3`
 `;
 
 const Detail = styled.div`
+  display: flex;
   margin-bottom: 5px;
 `;
 
 const Value = styled.span`
+  margin-left: 5px;
   text-transform: capitalize;
   font-weight: ${({ theme }) => theme.light};
 `;
 
-const Card = ({ countryName, population, region, capital, flag }) => {
+const Card = ({
+  isVisible,
+  countryName,
+  population,
+  region,
+  capital,
+  flag,
+}) => {
   const slugifiedName = slugify(countryName, {
     lower: true,
   });
   return (
-    <Wrapper to={`/${slugifiedName}`}>
+    <Wrapper visible={isVisible.toString()} to={`/${slugifiedName}`}>
       <ImageWrapper>
         <Image src={flag} alt={countryName} />
       </ImageWrapper>
       <Content>
         <Title>{countryName}</Title>
         <Detail>
-          Population:{' '}
+          Population:
           <Value>
             {population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}
           </Value>
         </Detail>
         {region && (
           <Detail>
-            Region: <Value>{region}</Value>
+            Region:<Value>{region}</Value>
           </Detail>
         )}
         {capital && (
           <Detail>
-            Capital: <Value>{capital}</Value>
+            Capital:<Value>{capital}</Value>
           </Detail>
         )}
       </Content>
@@ -84,6 +96,7 @@ const Card = ({ countryName, population, region, capital, flag }) => {
 };
 
 Card.propTypes = {
+  isVisible: PropTypes.bool.isRequired,
   countryName: PropTypes.string.isRequired,
   population: PropTypes.number.isRequired,
   region: PropTypes.string.isRequired,
